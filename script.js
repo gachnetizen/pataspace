38711e1cbe81478c56fd18bf53cd1e0579d45090
Ruth Gacheri <rgacheri2005@gmail.com>
2026-06-02 20:32:17 +0300

    Assignment 1

--- script.js
+++
@@ -1,6 +0,0 @@
-let greetButton = document.getElementById("greetBtn");
-
-greetButton.addEventListener("click", function() {
-    let name = document.getElementById("nameInput").value;
-    document.getElementById("output").innerHTML = "Hello " + name + "!";
-});

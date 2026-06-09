fef987e213032bf0a6925db88ea1840947061f47
Ruth Gacheri <rgacheri2005@gmail.com>
2026-06-09 20:41:36 +0300

    Initial commit: add Assignment project and bossb

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main_Calc(string[] args)
+        {
+            Console.WriteLine(" Simple Calculator");
+
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator (+, -, *, /): ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                    {
+                        Console.WriteLine("Error: Cannot divide by zero.");
+                        return;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator.");
+                    return;
+            }
+
+            Console.WriteLine($"Result: {num1} {op} {num2} = {result}");
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
